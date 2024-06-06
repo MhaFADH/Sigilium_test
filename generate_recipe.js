badfdfaea85de0421a2e595f6fff2e3e2adc81bf
Mohamed-amine FADHLAOUI <fadhlaoui.mohameda@gmail.com>
2024-06-06 20:49:52 +0200

    chores: added some css for basic styling

--- generate_recipe.js
+++ generate_recipe.js
@@ -2,11 +2,11 @@ import recipe from "./recipe.json" with {type: "json"}
 
 const container = document.getElementsByClassName("container")[0];
 
-let mappedRecipe = "<div><h2>Ingredients</h2><div>"
+let mappedRecipe = "<div class='d-flex flex-column text-center'><h2><u>Ingredients</u></h2><div class ='d-flex flex-wrap'>"
 
 recipe.ingredients.forEach((ingredient) => {
   mappedRecipe += `
-  <div class="card" style="width: 18rem;">
+  <div class="card" style="width:15%">
     <img class="card-img-top" src="pictures/${ingredient.picture}" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">${ingredient.translated_name[0].fr}</h5>
@@ -17,11 +17,11 @@ recipe.ingredients.forEach((ingredient) => {
   ;
 })
 
-mappedRecipe += "</div><h2>Temps de préparation</h2><div>"
+mappedRecipe += "</div><h2><u>Temps de préparation</u></h2><div class='d-flex flex-column align-items-center'>"
 
 Object.entries(recipe.timing).forEach(([key,value]) => {
   mappedRecipe += `
-  <div class="card" style="width:18rem;">
+  <div class="card" style="width:44%">
     <div class="card-body">
       <h5 class="card-title">${key.toUpperCase()} - ${value.quantity} ${value.unit}</h5>
     </div>
@@ -29,10 +29,10 @@ Object.entries(recipe.timing).forEach(([key,value]) => {
   `}
 )
 
-mappedRecipe += "</div><h2 >Etapes</h2><div>"
+mappedRecipe += "</div><h2 ><u>Etapes</u></h2><div class='d-flex flex-column align-items-center'>"
 recipe.steps.forEach((step) => {
   mappedRecipe += `
-  <div class="card" style="width:44%;">
+  <div class="card" style="width:44%">
     <div class="card-body">
       <h5 class="card-title">Etape ${step.order}</h5>
       ${step.ingredients.length > 0 ? `<p> ingredients: ${step.ingredients.map(ingredientId => recipe.ingredients.find(ingredient => ingredient.id === ingredientId).translated_name[0].fr).join(", ")} </p>`:""}
