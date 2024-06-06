cef5137b30adbcc4925ef1b24b196a3912202f91
Mohamed-amine FADHLAOUI <fadhlaoui.mohameda@gmail.com>
2024-06-06 20:30:04 +0200

    feat: can now see the ingredients needed for the recipe

--- generate_recipe.js
+++ generate_recipe.js
@@ -1,3 +1,23 @@
 import recipe from "./recipe.json" with {type: "json"}
 
-console.log(recipe)
+const container = document.getElementsByClassName("container")[0];
+
+let mappedRecipe = "<div>"
+
+mappedRecipe += "<h2>Ingredients</h2><div>"
+recipe.ingredients.forEach((ingredient) => {
+  mappedRecipe += `
+  <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="pictures/${ingredient.picture}" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">${ingredient.translated_name[0].fr}</h5>
+      <p class="card-text">${ingredient.quantity} ${ingredient.unit}</p>
+    </div>
+  </div>
+  `
+  ;
+})
+mappedRecipe += "</div></div>"
+
+container.innerHTML = container.innerHTML.replace(
+  "REPLACEME",mappedRecipe);
